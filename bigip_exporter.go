38b16cc66c4e345384921254b2a5758a1f350875
Christoffer Gunning <christoffer.gunning@bonniernews.se>
2017-01-03 15:56:42 +0100

    Implementing use of viper and pflags for configuration

--- bigip_exporter.go
+++ bigip_exporter.go
@@ -24,32 +24,31 @@ func listen(exporter_bind_address string, exporter_bind_port int) {
 			</html>`))
 	})
 	exporter_bind := exporter_bind_address + ":" + strconv.Itoa(exporter_bind_port)
-	log.Fatal(http.ListenAndServe(exporter_bind, nil))
+	log.Fatalf("Process failed: %s", http.ListenAndServe(exporter_bind, nil))
 }
 
 func main() {
 	config := config.GetConfig()
-
-	if config.Exporter_config.Exporter_debug {
+	if config.Exporter.LogLevel == "debug" {
 		log.Printf("Config: %v", config)
 	}
 
-	bigip_endpoint := config.Bigip_config.Bigip_host + ":" + strconv.Itoa(config.Bigip_config.Bigip_port)
+	bigip_endpoint := config.Bigip.Host + ":" + strconv.Itoa(config.Bigip.Port)
 	var exporter_partitions_list []string
-	if config.Exporter_config.Exporter_partitions != "" {
-		exporter_partitions_list = strings.Split(config.Exporter_config.Exporter_partitions, ",")
+	if config.Exporter.Partitions != "" {
+		exporter_partitions_list = strings.Split(config.Exporter.Partitions, ",")
 	} else {
 		exporter_partitions_list = nil
 	}
 	auth_method := f5.TOKEN
-	if config.Bigip_config.Bigip_basic_auth {
+	if config.Bigip.BasicAuth {
 		auth_method = f5.BASIC_AUTH
 	}
 
-	bigip := f5.New(bigip_endpoint, config.Bigip_config.Bigip_username, config.Bigip_config.Bigip_password, auth_method)
+	bigip := f5.New(bigip_endpoint, config.Bigip.Username, config.Bigip.Password, auth_method)
 
-	_, bigipCollector := collector.NewBigIpCollector(bigip, config.Exporter_config.Exporter_namespace, exporter_partitions_list)
+	_, bigipCollector := collector.NewBigIpCollector(bigip, config.Exporter.Namespace, exporter_partitions_list)
 
 	prometheus.MustRegister(bigipCollector)
-	listen(config.Exporter_config.Exporter_bind_address, config.Exporter_config.Exporter_bind_port)
+	listen(config.Exporter.BindAddress, config.Exporter.BindPort)
 }
