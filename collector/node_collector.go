2fd2061e1bf0a996ba69271b5a23ed568e8f50e8
Christoffer Gunning <christoffer.gunning@bonniernews.se>
2017-01-03 17:43:58 +0100

    Updating vendor files and changing scrape status to a gauge

--- collector/node_collector.go
+++ collector/node_collector.go
@@ -12,7 +12,7 @@ type nodeCollector struct {
 	metrics                   map[string]nodeMetric
 	bigip                     *f5.Device
 	partitions_list           []string
-	collector_scrape_status   *prometheus.CounterVec
+	collector_scrape_status   *prometheus.GaugeVec
 	collector_scrape_duration *prometheus.SummaryVec
 }
 
@@ -153,8 +153,8 @@ func NewNodeCollector(bigip *f5.Device, namespace string, partitions_list []stri
 				valueType: prometheus.GaugeValue,
 			},
 		},
-		collector_scrape_status: prometheus.NewCounterVec(
-			prometheus.CounterOpts{
+		collector_scrape_status: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
 				Namespace: namespace,
 				Name:      "collector_scrape_status",
 				Help:      "collector_scrape_status",
